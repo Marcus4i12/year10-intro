e80549d2f54ef859c09997afb23a82008dfaee6d
Marcus Villegas <marcusvillegas08@yahoo.com>
2023-06-19 11:15:26 +0100

    adding read filet

---
+++ main.py
@@ -0,0 +1,15 @@
+from random import randint
+x = randint(1, 100)
+number = int(input("pick a number 1-100 "))
+tries = 1
+while (x!=number):
+    if x > number:
+        print("bigger")
+        number = int(input("try again "))
+    elif x < number:
+        print("smaller")
+        number = int(input("try again "))
+    tries = tries + 1
+print(tries, " attempts")
+
+print(x)
