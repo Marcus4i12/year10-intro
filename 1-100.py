b0c3bac5cbd33346f7b9c3ffd0d1d6a8715249ed
Marcus Villegas <marcusvillegas08@yahoo.com>
2023-06-20 09:56:41 +0100

    updated code

--- 1-100.py
+++ 1-100.py
@@ -1,13 +1,17 @@
 from random import randint
 x = randint(1, 100)
-number = int(input("pick a number 1-100 "))
 tries = 1
+number = "x"
 while (x!=number):
-    if x > number:
-        print("bigger")
-        number = int(input("try again "))
-    elif x < number:
-        print("smaller")
-        number = int(input("try again "))
+    try:
+        number = int(input("pick a number 1-100 "))
+        if x == number:
+            print("Correct")
+        elif x < number:
+            print("smaller")
+        elif x > number:
+            print("Bigger")
+    except ValueError:
+        print("Invalid input")
     tries = tries + 1
 print(tries, " attempts")
