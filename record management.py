58b6c0b1e2928d8bb742ec57f79a3f28bee7e02c
Marcus Villegas <marcusvillegas08@yahoo.com>
2023-06-21 15:18:16 +0100

    added line deletion

--- record management.py
+++ record management.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from pathlib import Path
 
 class records:
@@ -12,11 +11,11 @@ class records:
             file = open(r"c:\Users\training.user\Downloads\record.txt", "x") #opens anyway
 
         if option.lower() == "a":
-            opt1 = str(input("Enter your name: "))
-            opt2 = str(input("Whats your favourite sport? "))
-            opt3 = str(input("What is your favourite colour? "))
-            with open(r"c:\Users\training.user\Downloads\record.txt", "a") as writerecord:
-                writerecord.write(f"|\t{opt1}\t\t{opt2}\t\t\t{opt3}\n")            #add data to record
+                opt1 = str(input("Enter your name: "))
+                opt2 = str(input("Whats your favourite sport? "))
+                opt3 = str(input("What is your favourite colour? "))
+                with open(r"c:\Users\training.user\Downloads\record.txt", "a") as writerecord:
+                    writerecord.write(f"|\t{opt1}\t\t{opt2}\t\t\t{opt3}\n")            #add data to record
 
         elif option.lower() == "b":
             file = open(r"c:\Users\training.user\Downloads\record.txt", "r")        #shows inside
@@ -24,6 +23,19 @@ class records:
             file.close()
 
         elif option.lower() == "c":
+            def delete_line(file_name, line_number):
+                with open(file_name, "r") as f:
+                    lines = f.readlines()
+                with open(file_name, "w") as f:
+                    for i in range(len(lines)):
+                        if i != line_number:
+                            f.write(lines[i])
+            if __name__ == "__main__":
+                file_name = r"c:\Users\training.user\Downloads\record.txt"
+                line_number = int(input("Enter the line number to delete: "))
+                delete_line(file_name, line_number)
+
+        elif option.lower() == "d":
             print("Deleted content")
             file = open(r"c:\Users\training.user\Downloads\record.txt", "r+")
             file.truncate(0)        #removes data at stated value
@@ -49,8 +61,9 @@ while True:
     print("Pick an option:")
     print("a: add record")
     print("b: show record")
-    print("c: delete record")
+    print("c: remove a record")
+    print("d: delete all records")
     print()
 
-    option = input("Choose an option: a,b,c: ").upper()
+    option = input("Choose an option: a,b,c,d: ").upper()
     records(option)         #loop code
